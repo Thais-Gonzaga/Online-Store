18021d981614392615a84332e87025f35c4d9be3
Cmcardosoo <c.marceloo@me.com>
2022-10-07 17:52:43 -0300

    Requisito 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import MainPage from './components/MainPage';
+import ShoppingCart from './components/ShoppingCart';
 
 class App extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ MainPage } />
+          <Route path="/cart" component={ ShoppingCart } />
         </Switch>
       </BrowserRouter>
     );
